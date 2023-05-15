ad3f7122f910047673fde0a7f528ba051476a0b4
nick <nick@feverdreams.io>
2023-05-14 20:29:12 -0500

    migrate client to client folder

---
+++ packages/client/src/index.js
@@ -0,0 +1,21 @@
+import { ColorModeScript } from "@chakra-ui/color-mode";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </BrowserRouter>  
+    </ChakraProvider>
+
+
+  </React.StrictMode>,
+  document.getElementById("root")
+);
